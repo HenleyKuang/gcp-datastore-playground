ab917f6d3a26fbbf27dc3c7b9ee727b95b33864f
Henley Kuang <hkuang@brightedge.com>
2019-09-25 13:51:44 -0700

    get specific document

--- gcp_datastore_playground/gcp_datastore_playground.py
+++ gcp_datastore_playground/gcp_datastore_playground.py
@@ -16,7 +16,6 @@ def _parse_args():
     datastore_parser = argparse.ArgumentParser(add_help=False)
     datastore_parser.add_argument('--service-key-path',
                                   action='store',
-                                  required=True,
                                   help='service key json path')
 
     command_subparser = parser.add_subparsers(dest='client_type')
@@ -32,11 +31,20 @@ def _main():
     client_type = options.client_type
 
     if client_type == "datastore":
+        db = None
+        # Using firestore
         service_key_path = options.service_key_path
-        db = firestore.Client.from_service_account_json(service_key_path)
-        docs = db.collection(u'test-henley').stream()
-        for doc in docs:
-            LOGGER.info('{} => {}'.format(doc.id, doc.to_dict()))
+        if service_key_path:
+            db = firestore.Client.from_service_account_json(service_key_path)
+        else:
+            db = firestore.Client()
+        start_time = time.time()
+        doc_ref = db.collection('test-henley').document('34')
+        doc = doc_ref.get()
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        LOGGER.info('Document data: {} | Elapsed time: {}'.format(
+            doc.to_dict(), elapsed_time))
 
 
 if __name__ == "__main__":
